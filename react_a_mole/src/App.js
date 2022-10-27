fba6e9d70b8298d9f0726757559526d2f251b3c3
Susan Heller <*jefa8@gmail.com>
2022-10-27 09:58:17 -0400

    working copy, requires notations

--- react_a_mole/src/App.js
+++ react_a_mole/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+// IMPORTS
 import './App.css';
+import { useState } from 'react'
+import MoleContainer from './components/MoleContainer'
 
+// MAIN FUNCTION
 function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = () => {
+    let hills = []
+    for(let i = 0; i < 9; i++) {
+      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
+    }
+    return (
+      <div>
+        { hills }
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-A-Mole!</h1>
+      {score}
+      {createMoleHill()}
     </div>
   );
 }
+// line 8 utilizies "useState" to start the score.  It is set to zero to begin.
+// line 10 declares "createMoleHill"
+// line 11 delcares "hills" as an empty array
+// line 12 "for" statement doing array iterates over an array
+// line 13 "hills.push"-- the "push()" method is used to add to the "hills" array
+// 
 
+
+// EXPORT
 export default App;
